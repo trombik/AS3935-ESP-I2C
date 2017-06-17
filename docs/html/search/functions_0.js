3e2272f4e36ddefc223208516562187c7dc5c39f
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-17 17:09:57 +0900

    sync

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['begin',['begin',['../classAS3935.html#a75542117963fb6a48f6bf257bca06c41',1,'AS3935::begin(void)'],['../classAS3935.html#a3ea6e01becd6faac61daf92ff6a8f7d3',1,'AS3935::begin(int sda, int scl)']]]
+];
