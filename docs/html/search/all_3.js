18f95974d9af44468097cad24bae73fa4fcf2a08
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-18 08:05:33 +0900

    sync

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['descresenoisefloor',['descreseNoiseFloor',['../classAS3935.html#abbed826cf7f83a4d598e20e964620d34',1,'AS3935']]],
   ['disableoscillators',['disableOscillators',['../classAS3935.html#affd3cfa6277c479da8e15d0f63c2240f',1,'AS3935']]]
 ];
--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['getdistance',['getDistance',['../classAS3935.html#a7757e3320432cf7aa037a56a204d5946',1,'AS3935']]],
   ['getintrruptreason',['getIntrruptReason',['../classAS3935.html#af8e6b140e4d495de1fde1586c9a037e3',1,'AS3935']]],
-  ['getminimumlightning',['getMinimumLightning',['../classAS3935.html#acb47e57b90db5fd7076e8f613c21d7d3',1,'AS3935']]]
+  ['getminimumlightning',['getMinimumLightning',['../classAS3935.html#acb47e57b90db5fd7076e8f613c21d7d3',1,'AS3935']]],
+  ['getnoisefloor',['getNoiseFloor',['../classAS3935.html#a27a053f6681f76fe822532796b32154b',1,'AS3935']]]
 ];
