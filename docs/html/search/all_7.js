20cea5027d381c0c8a3998725c58e26a89ce2900
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-18 07:16:15 +0700

    implement method to set internal capacitor values (#8)
    * implement method to set internal capacitor values
    
    fixes #2
    
    * regen

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -4,5 +4,6 @@ var searchData=
   ['setindoor',['setIndoor',['../classAS3935.html#a2dfca515cd05d3d8a2f21a63ecca9803',1,'AS3935::setIndoor(void)'],['../classAS3935.html#a50d484a2b9cf94415a53f85a739956c7',1,'AS3935::setIndoor(bool enable)']]],
   ['setminimumlightning',['setMinimumLightning',['../classAS3935.html#a101cee8826bf70b18a50bfbbe2bd2133',1,'AS3935']]],
   ['setnoisefloor',['setNoiseFloor',['../classAS3935.html#a423451d2316612a42e03ef51f601933f',1,'AS3935']]],
-  ['setoutdoor',['setOutdoor',['../classAS3935.html#a3f069dc7019193600bf5e0c1fd8a700c',1,'AS3935::setOutdoor(void)'],['../classAS3935.html#aea4888bce6d95ecd15455eb5c646ecf5',1,'AS3935::setOutdoor(bool enable)']]]
+  ['setoutdoor',['setOutdoor',['../classAS3935.html#a3f069dc7019193600bf5e0c1fd8a700c',1,'AS3935::setOutdoor(void)'],['../classAS3935.html#aea4888bce6d95ecd15455eb5c646ecf5',1,'AS3935::setOutdoor(bool enable)']]],
+  ['settuningcapacitor',['setTuningCapacitor',['../classAS3935.html#a36a66cea1b70e87dc97bde6bc0688c49',1,'AS3935']]]
 ];
